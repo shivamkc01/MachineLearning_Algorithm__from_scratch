64a0fb5f8cc9b9a7118a6b4c5768bca6068dbe99
ShivamChhetry <39437051+shivamkc01@users.noreply.github.com>
2021-08-10 22:30:16 +0530

    Add files via upload

---
+++ Perceptron_scratch.py
@@ -0,0 +1,126 @@
+"""
+The Perceptron is a linear machine learning algorithm for binary classification tasks.
+Perceptron Algorithm for Classification in Python
+by Jason Brownlee on December 11, 2020 in Python Machine Learning
+Tweet  Share
+The Perceptron is a linear machine learning algorithm for binary classification tasks.
+
+It may be considered one of the first and one of the simplest types of artificial neural networks. It is definitely not “deep” learning but is an important building block.
+
+Like logistic regression, it can quickly learn a linear separation in feature space for two-class classification tasks, although unlike logistic regression, it learns using the stochastic gradient descent optimization algorithm and does not predict calibrated probabilities.
+
+In this tutorial, you will discover the Perceptron classification machine learning algorithm.
+
+After completing this tutorial, you will know:
+
+The Perceptron Classifier is a linear algorithm that can be applied to binary classification tasks.
+How to fit, evaluate, and make predictions with the Perceptron model with Scikit-Learn.
+How to tune the hyperparameters of the Perceptron algorithm on a given dataset.
+Let’s get started.
+
+Perceptron Algorithm for Classification in Python
+Perceptron Algorithm for Classification in Python
+Photo by Belinda Novika, some rights reserved.
+
+Tutorial Overview
+This tutorial is divided into 3=three parts; they are:
+
+Perceptron Algorithm
+Perceptron With Scikit-Learn
+Tune Perceptron Hyperparameters
+Perceptron Algorithm
+The Perceptron algorithm is a two-class (binary) classification machine learning algorithm.
+
+It is a type of neural network model, perhaps the simplest type of neural network model.
+
+Activation = Weights * Inputs + Bias
+Predict 1: If Activation > 0.0
+Predict 0: If Activation <= 0.0
+weights(t + 1) = weights(t) + learning_rate * (expected_i – predicted_) * input_i
+
+Programmed by Shivam Chhetry
+** 10-08-2021
+"""
+# Imports Libraries
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+class Perceptron():
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.activation_func = self._unit_step_function
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        y_ = np.array([1 if i > 0 else 0 for i in y])
+
+        for _ in range(self.n_iters):
+
+            for idx, x_i in enumerate(X):
+
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                # Perceptron update rule
+                update = self.lr * (y_[idx] - y_predicted)
+
+                self.weights += update * x_i
+                self.bias += update
+
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_model)
+        return y_predicted
+
+    def _unit_step_function(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+
+if __name__ == '__main__':
+
+    X, y = datasets.make_blobs(
+        n_samples=150, n_features=2, centers=2, cluster_std=1.05, random_state=2
+    )
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123
+    )
+
+    # creating accuracy function
+    def accuracy(y_true, y_pred):
+        acu = np.sum(y_true == y_pred)/len(y_true)
+        return acu
+    p = Perceptron(learning_rate=0.01, n_iters=1000)
+    p.fit(X_train, y_train)
+    predictions = p.predict(X_test)
+
+    print("Perceptron classification accuracy", accuracy(y_test, predictions)*100)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
+
+    x0_1 = np.amin(X_train[:, 0])
+    x0_2 = np.amax(X_train[:, 0])
+
+    x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
+    x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
+
+    ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
+
+    ymin = np.amin(X_train[:, 1])
+    ymax = np.amax(X_train[:, 1])
+    ax.set_ylim([ymin - 3, ymax + 3])
+
+    plt.show()
